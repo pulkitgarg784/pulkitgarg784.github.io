9da848d2d003a100ce63381d2946c22d583b6d98
pulkitgarg784 <33174851+pulkitgarg784@users.noreply.github.com>
2020-05-26 09:25:25 +0530

    added links and fixed card height

--- Topnav.js
+++ Topnav.js
@@ -1,6 +1,6 @@
 function myFunction() {
     var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
+    if (x.className === "animated fadeIn delay-2s topnav" || x.className === "topnav" ) {
       x.className += " responsive";
     } else {
       x.className = "topnav";
